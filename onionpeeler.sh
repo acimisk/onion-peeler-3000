eed9cc5c5cb2eb7a2b6179ac014a5d587c74518b
Kenan ACIMIŞ <acimisk@gmail.com>
2026-07-29 02:51:53 +0300

    Add onionpeeler.sh for file type processing
    This script processes files based on their type, handling gzip, bzip2, tar, and ASCII text formats. It renames and extracts files accordingly, displaying a message when an ASCII text file is found.

---
+++ onionpeeler.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+file="$1"
+
+while true
+do
+    fileType=$(file "$file")
+
+    if echo "$fileType" | grep -q "gzip"; then
+        mv "$file" "$file.gz"
+        gunzip -f "$file.gz"
+        file=$(ls -t | grep -v "sogan.sh" | head -n 1)
+
+    elif echo "$fileType" | grep -q "bzip2"; then
+        mv "$file" "$file.bz2"
+        bunzip2 -f "$file.bz2"
+        file=$(ls -t | grep -v "sogan.sh" | head -n 1)
+
+    elif echo "$fileType" | grep -q "tar"; then
+        mv "$file" "$file.tar"
+        tar -xf "$file.tar"
+        rm "$file.tar"
+        file=$(ls -t | grep -v "sogan.sh" | head -n 1)
+
+    elif echo "$fileType" | grep -q "ASCII text"; then
+        echo ""
+        echo "==========================="
+        echo "   WE GOT HIM!!            "
+        echo "==========================="
+        cat "$file"
+        echo ""
+        exit 0
+
+    else
+        echo "Not my problem anymore: $fileType"
+        break
+    fi
+done
