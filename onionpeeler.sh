534d27eeaa383dc8e544931e113e9e7499f3f6be
Kenan ACIMIŞ <acimisk@gmail.com>
2026-07-29 17:55:05 +0300

    Update file exclusion from onionpeeler.sh

--- onionpeeler.sh
+++ onionpeeler.sh
@@ -9,18 +9,18 @@ do
     if echo "$fileType" | grep -q "gzip"; then
         mv "$file" "$file.gz"
         gunzip -f "$file.gz"
-        file=$(ls -t | grep -v "sogan.sh" | head -n 1)
+        file=$(ls -t | grep -v "onionpeeler.sh" | head -n 1)
 
     elif echo "$fileType" | grep -q "bzip2"; then
         mv "$file" "$file.bz2"
         bunzip2 -f "$file.bz2"
-        file=$(ls -t | grep -v "sogan.sh" | head -n 1)
+        file=$(ls -t | grep -v "onionpeeler.sh" | head -n 1)
 
     elif echo "$fileType" | grep -q "tar"; then
         mv "$file" "$file.tar"
         tar -xf "$file.tar"
         rm "$file.tar"
-        file=$(ls -t | grep -v "sogan.sh" | head -n 1)
+        file=$(ls -t | grep -v "onionpeeler.sh" | head -n 1)
 
     elif echo "$fileType" | grep -q "ASCII text"; then
         echo ""
